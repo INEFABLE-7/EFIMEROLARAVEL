8ff942ea341ea35bd76511f65e0ff274a719e0ed
INEFABLE-7 <1460446@senati.pe>
2023-10-13 09:22:55 -0500

    barra de menu de pagina de turismo huanuco

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+
 </head>
 <body>
-    <h1>HOLA NO PUDE CORREGIR EL ERROR DE XAMP ASI QUE DECIDI TRABAJAR CON LARAGON :( </h1>
+<div class="navbar-fixed">
+    <nav class="indigo z-depth-5">
+    <div class="nav-wrapper">
+        <div class="container">
+        <a href="#" class="brand-logo"><i class="material-icons">linked_camera</i>TURISMO HUANUCO</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Lugares</a></li>
+            <li><a href="badges.html">Visitantes</a></li>
+            <li><a href="collapsible.html">reservaciones </a></li>
+            <li><a href="collapsible.html">Guia de acompañante</a></li>
+        </ul>
+        </div>
+        </div>
+    </nav>
+</div>
 </body>
 </html>
