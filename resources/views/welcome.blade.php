ca8d547b71a304ad529071e8568e8dc7897efeac
INEFABLE-7 <1460446@senati.pe>
2023-10-13 13:33:55 -0500

    agregamos un iframe para mostrar una ventana externa de mapa para una mejor vista del usuario un mapa de huanuco  y asi hacer mas interactivo la pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,5 +44,22 @@
     </div>
   </div>
 </div>
+
+<div class="row z-depth-5">
+    <div class="col s12">
+    <div class="card-panel teal">
+      <iframe class="" width="100%" height="600px" frameBorder="0" allowfullscreen src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1979.8872380577985!2d-76.24549884836048!3d-9.932257745584898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91b1b726d15e2c2b%3A0xb07c6603cc2f8a6c!2sHu%C3%A1nuco%2C%20Per%C3%BA!5e0!3m2!1sen!2sus!4v1634126196330!5m2!1sen!2sus"></iframe>
+    </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
 </body>
 </html>
